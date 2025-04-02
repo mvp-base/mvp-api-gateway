f879cce3b3e197601403d15bf8bdb92d28786550
vanm30 <vanikmatej99@gmail.com>
2025-04-02 18:10:49 +0000

    bind to ipv4

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
-  await app.listen(process.env.PORT ?? 9000);
+  await app.listen(process.env.PORT ?? 9000, '0.0.0.0');
   logger.info(`Application is running on: ${await app.getUrl()}`);
 }
 void bootstrap();
