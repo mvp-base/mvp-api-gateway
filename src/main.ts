6ef084445f75b7bef2e99fc136c59769432c119b
vanm30 <vanikmatej99@gmail.com>
2025-04-02 19:51:54 +0000

    cors to domain

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,26 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
 
+  const allowedOrigins = [/\.matejv\.com$/, 'https://matejv.com']; // Allow matejv.com & all subdomains
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (
+        !origin ||
+        allowedOrigins.some((allowed) =>
+          allowed instanceof RegExp ? allowed.test(origin) : allowed === origin,
+        )
+      ) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'), false);
+      }
+    },
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   await app.listen(process.env.PORT ?? 9000, '0.0.0.0');
